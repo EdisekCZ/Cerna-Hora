c66683336b6a488c8d937708efee6bf9dab1460e
unknown <edisekcz@gmail.com>
2021-04-13 22:12:44 +0200

    Dokončení úvodní stránky

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#poloha').on('click',function(){
+        $(this).attr("src", "img/poloha2.jpg");
+    });
+});
+$(document).ready(function(){
+    $('#nadpis').on('click',function(){
+        $(this).text("Montenegro");
+    })
+});
